50c9ad1763877ce9613b8e3b97cea744be5228ab
Gaurav Sutar <gauravstr05@gmail.com>
2022-10-08 15:27:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log('Calling app.js')
+
+let userDetails = {
+    name: "Gaurav-Sutar",
+    age: 22,
+    designation : "Software Developer",
+    printDetails: function(){
+        console.log(this)
+    }
+}
+userDetails.printDetails();
+
+
+let userDetails2 = {
+    name: "Tushar-Manker",
+    age: 22,
+    exprience: 1,
+    designation : "Software Developer",
+    printDetails: function(){
+        console.log(this)
+    }
+}
+
+userDetails.printDetails.call(userDetails2);
