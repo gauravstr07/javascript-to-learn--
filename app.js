bbf6db03e6077dc572bbd3e30a37126d3c789636
Gaurav Sutar <gauravstr05@gmail.com>
2022-10-08 15:36:46 +0530

    Call Apply Blind

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-console.log('Calling app.js')
+console.log("Calling app.js");
 
 let userDetails = {
-    name: "Gaurav-Sutar",
-    age: 22,
-    designation : "Software Developer",
-    printDetails: function(){
-        console.log(this)
-    }
-}
+  name: "Gaurav-Sutar",
+  age: 22,
+  designation: "Software Developer",
+  printDetails: function () {
+    console.log(this);
+  },
+};
 userDetails.printDetails();
 
-
 let userDetails2 = {
-    name: "Tushar-Manker",
-    age: 22,
-    exprience: 1,
-    designation : "Software Developer",
-    printDetails: function(){
-        console.log(this)
-    }
-}
+  name: "Tushar-Manker",
+  age: 22,
+  exprience: 1,
+  designation: "Software Developer",
+  printDetails: function () {
+    console.log(this);
+  },
+};
 
 userDetails.printDetails.call(userDetails2);
